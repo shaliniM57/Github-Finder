4b1b45fda012e7b5a54fddebf8fe579b5a08054b
shaliniM57 <111411444+shaliniM57@users.noreply.github.com>
2022-09-08 22:59:43 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const About= ()=> {
+    return (
+        <div className="my-5">
+            <p>Github Finder is an React App  that allows  to find the github users and watch their profile </p>
+        </div>
+    )
+}
+export default About
